3fb9631d6e7e4d86886078d81c15dfa2c67cd501
Andrii Zadvornyi <azadvornyi@deloitte.com>
2022-10-13 20:55:56 +0300

    (feature) add some BDD tests

--- app/libs/Calculator.spec.ts
+++ app/libs/Calculator.spec.ts
@@ -3,10 +3,35 @@ import {Calculator} from './Calculator';
 describe('Test suite for Calculator.ts', () => {
   let calculator;
   beforeAll(() => {
+    let inputResult = document.createElement("input");
+    inputResult.type = "text";
+    inputResult.id = 'dashboard'
+    inputResult.className = "app-result";
+
     calculator = new Calculator()
+    calculator.dashboard = inputResult;
+  });
+
+  it('printDigit should to be defined', () => {
+    expect(calculator.printDigit).toBeDefined();
+  });
+
+  it('printDigit should add new value', () => {
+    calculator.printDigit('5');
+    calculator.printDigit('5');
+    expect(calculator.dashboard.value).toBe('55');
+  });
+
+  it('printDigit should to be call in calculator.paste', () => {
+    const onSpy = jest.spyOn(calculator, 'printDigit');
+    calculator.paste()
+    expect(onSpy).toHaveBeenCalled();
   });
 
   it('printAction should to be defined', () => {
     expect(calculator.printAction).toBeDefined();
   });
+
 });
+
+
